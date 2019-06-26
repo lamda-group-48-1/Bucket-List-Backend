df2fdebee7bb94f25893445ba3d6e4b610b53058
EZEKIEL ILORI <ilorieazykiel@gmail.com>
2019-06-26 12:50:36 +0100

    implements get a bucketlist route

--- server/controllers/getList.js
+++ server/controllers/getList.js
@@ -1,7 +1,7 @@
 import { getLists } from '../utilities/db';
 import { handleError, handleResponse } from '../utilities';
 
-const getBucketLists = async (req, res) => {
+export const getBucketLists = async (req, res) => {
   try {
     const rows = await getLists([req.userId]);
     handleResponse(res, rows, 200);
@@ -10,4 +10,19 @@ const getBucketLists = async (req, res) => {
     handleError(res, errorMessage, 500);
   }
 };
-export default getBucketLists;
+
+export const getBucketList = async (req, res) => {
+  try {
+    const rows = await getLists([req.userId], req.params.id);
+
+    if (rows.length) {
+      handleResponse(res, rows, 200);
+    } else {
+      const errorMessage = 'Bucketlist not found';
+      handleError(res, errorMessage, 404);
+    }
+  } catch (error) {
+    const errorMessage = `SERVER ERROR: ${error.message}`;
+    handleError(res, errorMessage, 500);
+  }
+};
